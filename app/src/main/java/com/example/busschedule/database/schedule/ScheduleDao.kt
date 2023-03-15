310c2a96da321f6c319ee5c24456d5a0d113df9c
gichuhiii <gnwcocoe@gmail.com>
2023-03-15 11:37:36 +0300

    Completed

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -2,15 +2,17 @@ package com.example.busschedule.database.schedule
 
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ScheduleDao {
 
     //show all bus stops in ascending order by arrival time
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
-    fun getAll(): List<Schedule>
+    //use flow to allow DAO to continuously emit data from the database and send it back to the fragment
+    fun getAll(): Flow<List<Schedule>>
 
     //select all columns where results match selected stop name in the schedule table
     @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
-    fun getByStopName(stopName: String): List<Schedule>
+    fun getByStopName(stopName: String): Flow<List<Schedule>>
 }
--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -3,6 +3,7 @@ package com.example.busschedule.viewmodels
 import androidx.lifecycle.ViewModel
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
+import kotlinx.coroutines.flow.Flow
 
 // use View model to separate the responsibility of loading data from your activity or fragmnet
 //ViewModel is used to store data related to an app's UI and is also lifecycle aware
@@ -10,8 +11,8 @@ import com.example.busschedule.database.schedule.ScheduleDao
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
 
     // method to get the full schedule
-    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+    fun fullSchedule(): Flow<List<Schedule>> = scheduleDao.getAll()
 
     //method to get the schedule for a specific stop
-    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun scheduleForStopName(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 }
